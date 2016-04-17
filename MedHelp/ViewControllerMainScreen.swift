4c125f0a084a1998ec90a7ee7a4320f07f54e581
Luiz Daniel Ramos Franca <luizdaniel.r.f@gmail.com>
2016-04-16 23:47:47 -0300

    Create singup screen

---
+++ MedHelp/ViewControllerMainScreen.swift
@@ -0,0 +1,35 @@
+//
+//  ViewControllerMainScreen.swift
+//  MedHelp
+//
+//  Created by Luiz Daniel Ramos Franca on 4/16/16.
+//  Copyright © 2016 MedHelp. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerMainScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MedHelp/ViewControllerSingUp.swift
@@ -0,0 +1,54 @@
+//
+//  ViewControllerSingUp.swift
+//  MedHelp
+//
+//  Created by Luiz Daniel Ramos Franca on 4/16/16.
+//  Copyright © 2016 MedHelp. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerSingUp: UIViewController {
+
+    @IBOutlet weak var NameTextField: UITextField!
+    @IBOutlet weak var EmailTextField: UITextField!
+    @IBOutlet weak var PasswordTextField: UITextField!
+    @IBOutlet weak var ConfirmPasswordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    /**
+     * Called when the user click on the view (outside the UITextField).
+     */
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+
+}
